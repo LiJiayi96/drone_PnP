b1c886cfa5f908b6fae92f8839625d53c0b210ba
ljy <jiayili96@gmail.com>
2019-03-17 19:46:44 +0800

    drone_pnp

---
+++ drone_pnp/cmake-build-debug/CMakeFiles/drone_pnp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drone_pnp.dir/main.cpp.o"
+  "drone_pnp.pdb"
+  "drone_pnp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drone_pnp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
